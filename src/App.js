bb9a81333b5497e836f6d83076f9803848bb83b8
Lauren Forcey <lauren4c@gmail.com>
2018-12-28 09:53:05 -0700

    Completed Checkpoint 2764 and added room name list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from "./components/RoomList";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDvi7hoqxwVeCwFOEpzh5oSP09f0Ryk07Q",
+  authDomain: "c-bloc-chat.firebaseapp.com",
+  databaseURL: "https://c-bloc-chat.firebaseio.com",
+  projectId: "c-bloc-chat",
+  storageBucket: "c-bloc-chat.appspot.com",
+  messagingSenderId: "271758643817"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <aside className="room-list">
+          <RoomList firebase={firebase} />
+        </aside>
       </div>
     );
   }
